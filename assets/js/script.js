3b321ecbca57d94a190a5e0191fd216a978e46f1
Luan Cordeiro <luam.lc19@gmail.com>
2022-10-22 19:54:04 -0300

    Adicionando o repositório slider

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+function moveSlide(position) {
+    document.querySelector('.slider-container').style.left = position;
+}
+
+let a =document.querySelector('.slider-container');
+
+// setInterval(() => {a.style.left = '0px'}, 5000)
